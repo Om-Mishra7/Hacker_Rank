bdb7c23e38f455c1726d0184f4f59ec2ffb0a790
Om Mishra <admin@projectrexa.ml>
2023-02-27 09:19:36 +0530

    Day_2

--- Week_1/Day_2/Min-Max_Sum.py
+++ Week_1/Day_2/Min-Max_Sum.py
@@ -1,9 +1,14 @@
 def miniMaxSum(arr):
+    
+    # Sorting the array
     sorted_array = arr.sort()
+
     min_sum = sum(arr[0:4])
     max_sum = sum(arr[1:5])
+    
     print(min_sum, max_sum)
 
+# Sample Input
 
 arr = [1,3,5,7,9]
 
